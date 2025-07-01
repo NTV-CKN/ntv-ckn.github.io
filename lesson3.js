cccee36460866cea6512ebef811d47ff9e02e8c8
vuxinfix05 <nguyentruongvu000111@gmail.com>
2025-07-01 22:47:33 +0700

    complete

---
+++ lesson3.js
@@ -0,0 +1,20 @@
+let num1 = 9
+let num2 = 4
+let num3 = num1 - num2
+
+let cars =[]
+cars.push(3)
+cars = [1]
+
+let x = 4, y ="";
+
+console.log("Hello num1 ' dcsd'" + num1,1,2,3,45,5);
+console.log("hello " + ["sas", 2] + " cars " + cars);
+
+console.log("scscs",  's' + 2 +"dsds" +2, +3);
+
+
+console.log(num2);
+console.log(num3);
+
+
