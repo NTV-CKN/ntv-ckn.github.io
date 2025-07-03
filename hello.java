cad210c315c8f7db0a64bcb552c9d685d6012fee
vuxinfix05 <nguyentruongvu000111@gmail.com>
2025-07-03 13:43:10 +0700

    complete

--- hello.java
+++ hello.java
@@ -1,6 +1,8 @@
-class hello{
+class hello1{
    
 public static void main(String[] args) {
+    String str = "sxjsbcjsbcjbcjbcjcnsjkcn"+
+    "sncjkschks";
     System.out.println(4+5+"sxsxs Scxs' sccs\"sxs\" ");
 } 
 }
---
+++ lesson5_function_string.js
@@ -0,0 +1,5 @@
+let str = "Assssst";
+
+console.log(str.charCodeAt(0));
+console.log(str.slice(0, str.length));
+
