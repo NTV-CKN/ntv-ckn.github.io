403c0772a0d56d6b8b13835915ce02ac92cee1d2
vuxinfix05 <nguyentruongvu000111@gmail.com>
2025-07-03 14:10:08 +0700

    complete

--- lesson5_function_string.js
+++
@@ -1,5 +0,0 @@
-let str = "Assssst";
-
-console.log(str.charCodeAt(0));
-console.log(str.slice(0, str.length));
-
---
+++ lesson6_number.js
@@ -0,0 +1,28 @@
+let a = 6
+let b = "5"
+let c = b * 1
+
+let str1 = String("abc")
+let str2 = String("abc")
+
+console.log("str1 == str2 ", str1 == str2, " str1 === str2 ", str1 ===str2 );
+
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log(a - b);
+
+console.log(b + a);
+console.log(b - a); 
+console.log(b / a); 
+console.log(b * a);
+
+console.log(b + b);
+console.log(b * b);
+console.log(b / b);
+console.log(b - b);
+
+console.log("b", typeof b, " c ", typeof c);
+
+
