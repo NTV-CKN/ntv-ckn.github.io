effd56d4318c49b82f06577e4fc63c6f2d15a21c
vuxinfix05 <nguyentruongvu000111@gmail.com>
2025-07-12 18:46:50 +0700

    completed 12/7/2025 6:46pm

---
+++ lesson13_if_else.js
@@ -0,0 +1,11 @@
+let arr = ["ssxsx", "lll", "aaaaa", "wwww"]
+
+if (arr.length > 4) {
+    console.log("Nothing");
+} else {
+    if (arr.indexOf("wwww") == 4) {
+        console.log("Nothing");
+    } else {
+        console.log("End");
+    }
+}
