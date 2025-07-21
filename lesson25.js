15a03bcd9b24859735bd8c6d1fed08dc65ea9ae2
vuxinfix05 <nguyentruongvu000111@gmail.com>
2025-07-21 20:46:51 +0700

    completed 21/7/2025 8:46pm

--- lesson25.js
+++
@@ -1,8 +0,0 @@
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-        console.log("Result OK", this.responseText);
-    }
-};//callback
-xhttp.open("GET", "https://jsonplaceholder.typicode.com/todos", true);
-xhttp.send();
---
+++ lesson26.js
@@ -0,0 +1,13 @@
+/*
+https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Status
+
+Nhóm mã trạng thái HTTP
+
+Nhóm mã | Tên gọi        | Ý nghĩa chính
+--------|----------------|------------------------------------------------------------
+1xx     | Informational  | Phản hồi tạm thời – máy chủ đã nhận yêu cầu và đang tiếp tục xử lý
+2xx     | Successful     | Yêu cầu đã được xử lý thành công
+3xx     | Redirection    | Cần thực hiện thêm hành động (thường là chuyển hướng đến URL khác)
+4xx     | Client Error   | Lỗi từ phía client – cú pháp sai, không có quyền, tài nguyên không tồn tại
+5xx     | Server Error   | Lỗi từ phía máy chủ – server gặp sự cố khi xử lý yêu cầu hợp lệ
+*/
